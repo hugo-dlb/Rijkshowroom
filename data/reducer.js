4aa3bc08d3153dfc2ca6634b8801d191315e9514
turgodi <hugo.dalboussiere@gmail.com>
2018-12-04 22:11:10 +0100

    update store + create API data accessors

--- data/reducer.js
+++ data/reducer.js
@@ -1,23 +1,51 @@
+export const RESET_ARTWORK_DETAILS = 'RESET_ARTWORKS';
+export const GET_ARTWORK_DETAILS = 'GET_ARTWORK_DETAILS';
+export const GET_ARTWORK_DETAILS_SUCCESS = 'GET_ARTWORK_DETAILS_SUCCESS';
+export const GET_ARTWORK_DETAILS_FAIL = 'GET_ARTWORK_DETAILS_FAIL';
+
+export const RESET_ARTWORKS = 'RESET_ARTWORKS';
 export const GET_ARTWORKS = 'GET_ARTWORKS';
 export const GET_ARTWORKS_SUCCESS = 'GET_ARTWORKS_SUCCESS';
 export const GET_ARTWORKS_FAIL = 'GET_ARTWORKS_FAIL';
+export const ARTWORKS_PER_PAGE = 10;
 
 const DEFAULT_STATE = {
     artworks: [],
-    loading: false,
+    requestedPage: 0,
+    loading: true,
+    detailsLoading: true,
     error: null
 };
 
 export default function reducer(state = DEFAULT_STATE, action) {
     switch (action.type) {
+        case RESET_ARTWORKS:
+            return { ...state, artworks: []};
         case GET_ARTWORKS:
-            return { ...state, loading: true };
+            return { ...state, loading: true, requestedPage: action.payload.requestedPage };
+        case RESET_ARTWORK_DETAILS:
+            return { ...state, artworkDetails: null};
+        case GET_ARTWORK_DETAILS:
+            return { ...state, detailsLoading: true };
+        case GET_ARTWORK_DETAILS_SUCCESS:
+            return { ...state, detailsLoading: false, artworkDetails: action.payload.data };
+        case GET_ARTWORK_DETAILS_FAIL:
+            return {
+                ...state,
+                detailsLoading: false,
+                artworkDetails: null,
+                error: 'Error while fetching artwork details'
+            };
         case GET_ARTWORKS_SUCCESS:
-            return { ...state, loading: false, artworks: action.payload.data.artObjects };
+            const artworks = state.artworks;
+            artworks.push(...action.payload.data.artObjects);
+            return { ...state, loading: false, artworks: artworks };
         case GET_ARTWORKS_FAIL:
             return {
                 ...state,
                 loading: false,
+                artworks: [],
+                requestedPage: 0,
                 error: 'Error while fetching artworks'
             };
         default:
@@ -30,8 +58,32 @@ export function listArtworks(page = 1) {
         type: GET_ARTWORKS,
         payload: {
             request: {
-                url: '/collection/?key=zjIRbhGs&format=json&p=' + page
+                url: '/collection/?key=zjIRbhGs&format=json&p=' + page + '&ps=' + ARTWORKS_PER_PAGE
+            },
+            requestedPage: page
+        }
+    };
+}
+
+export function resetArtworks() {
+    return {
+        type: RESET_ARTWORKS
+    };
+}
+
+export function getArtworkDetails(objectNumber) {
+    return {
+        type: GET_ARTWORK_DETAILS,
+        payload: {
+            request: {
+                url: '/collection/' + objectNumber + '?key=zjIRbhGs&format=json'
             }
         }
+    };
+}
+
+export function resetArtworkDetails() {
+    return {
+        type: RESET_ARTWORK_DETAILS
     };
 }
